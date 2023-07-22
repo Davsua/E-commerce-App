df28f1d22982787847eb86e253ec47ba3c4a30df
David Suarez <davidsuarezcarvajal98@gmail.com>
2023-07-22 16:09:23 -0500

    login actualized

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -7,7 +7,13 @@ import { GetServerSideProps } from 'next';
 import { useForm } from 'react-hook-form';
 
 import { ErrorOutline } from '@mui/icons-material';
-import { Box, Grid, TextField, Button, Link, Chip, Divider } from '@mui/material';
+import { Box } from '@mui/material';
+import { Grid } from '@mui/material';
+import { TextField } from '@mui/material';
+import { Button } from '@mui/material';
+import { Link } from '@mui/material';
+import { Chip } from '@mui/material';
+import { Divider } from '@mui/material';
 import Typography from '@mui/material/Typography';
 
 import { AuthLayout } from '<@davsua>/components/layouts';
